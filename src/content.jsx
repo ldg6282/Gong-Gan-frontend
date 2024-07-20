ff7b643a28e1f1f7848c11ad3dfb9b94bbc10fdb
ldg <ldg6282@naver.com>
2024-07-21 03:47:32 +0900

    Feat: x-Frame-Options, CSP 헤더 제거 기능 구현 및 DOM 초기화 및 iframe 로드 기능 구현

--- src/content.jsx
+++ src/content.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "jotai";
 import App from "./App";
 import "./index.css";
 
 function initContentScript() {
+  if (document.head) {
+    document.head.remove();
+  }
+  if (document.body) {
+    document.body.remove();
+  }
+
+  const newHead = document.createElement("head");
+  const newBody = document.createElement("body");
+
+  document.documentElement.appendChild(newHead);
+  document.documentElement.appendChild(newBody);
+
+  const appRoot = document.createElement("div");
+  appRoot.id = "GONG-GAN-ROOT";
+  document.body.appendChild(appRoot);
+
   const rootElement = document.createElement("div");
   rootElement.id = "content-script-root";
-  document.body.appendChild(rootElement);
+  appRoot.appendChild(rootElement);
 
   const root = createRoot(rootElement);
   root.render(
-    <React.StrictMode>
+    <StrictMode>
       <Provider>
         <App />
       </Provider>
-    </React.StrictMode>,
+    </StrictMode>,
   );
 }
 
