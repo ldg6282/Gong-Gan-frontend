56100b62ba29f82e65aa0e63f9aa45c2411c251a
ldg6282 <ldg6282@naver.com>
2024-09-25 19:30:11 +0900

    Fix: chrome.runtime관련 로직 조건 수정

--- public/background.js
+++ public/background.js
@@ -5,19 +5,27 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "toggleExtension") {
     isExtensionActive = request.status;
     sendResponse({ status: isExtensionActive });
-  } else if (request.action === "checkExtensionActive") {
+    return true;
+  }
+  if (request.action === "checkExtensionActive") {
     sendResponse({ isActive: isExtensionActive });
-  } else if (request.action === "getRoomId") {
+    return true;
+  }
+  if (request.action === "getRoomId") {
     sendResponse({ roomId: currentRoomId });
-  } else if (request.action === "setRoomId") {
+    return true;
+  }
+  if (request.action === "setRoomId") {
     currentRoomId = request.roomId;
     sendResponse({ success: true });
-  } else if (request.action === "contentScriptLoaded") {
+    return true;
+  }
+  if (request.action === "contentScriptLoaded") {
     isExtensionActive = false;
     currentRoomId = null;
-  } else {
-    sendResponse({ error: "Unknown action" });
+    sendResponse({ success: true });
+    return true;
   }
-
+  sendResponse({ error: "Unknown action" });
   return true;
 });
